5ce2732b8f5a0c54ceeaffc1de026db889eafcf6
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-04-26 11:42:26 -0300

    copy

--- nuevo.js
+++ nuevo.js
@@ -1,3 +1,8 @@
 const options = {weekday: 'long' , day: 'numeric', month: 'long' , year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
+const yearSpan = document.querySelector("#CurrentYear");
+const currentYear = new Date();
+
+yearSpan.innerHTML = currentYear.getFullYear();
+console.log(currentYear);

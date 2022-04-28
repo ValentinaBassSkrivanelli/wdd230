158559e596d2377588f05d8077fc485334184a27
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-04-28 11:37:06 -0300

    js

--- javascript/nuevo.js
+++ javascript/nuevo.js
@@ -1,8 +1,7 @@
 const options = {weekday: 'long' , day: 'numeric', month: 'long' , year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
-const yearSpan = document.querySelector("#CurrentYear");
-const currentYear = new Date();
+const yearSpan = new Date()
+const currentYear = yearSpan.getFullYear();
 
-yearSpan.innerHTML = currentYear.getFullYear();
-console.log(currentYear);
+document.getElementById("currentYear").textContent = currentYear

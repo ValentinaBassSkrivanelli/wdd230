649a124d51411983cc2d81e3331c33b1ad55ce30
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-04-28 21:24:01 -0300

    designjava

---
+++ lesson2/js/design-java.js
@@ -0,0 +1,7 @@
+const options = {weekday: 'long' , day: 'numeric', month: 'long' , year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+const yearSpan = new Date()
+const currentYear = yearSpan.getFullYear();
+
+document.getElementById("currentYear").textContent = currentYear

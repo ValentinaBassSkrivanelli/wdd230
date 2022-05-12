564906d86780f6736dd14f54783d09c172997fde
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-05-12 17:18:07 -0300

    updated

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,7 +1,18 @@
 const options = {weekday: 'long' , day: 'numeric', month: 'long' , year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
+const option = {weekday: 'long' , day: 'numeric', month: 'long' , year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+document.getElementById('current').textContent = new Date().toLocaleDateString('en-US', option);
+
 const yearSpan = new Date()
 const currentYear = yearSpan.getFullYear();
 
 document.getElementById("currentYear").textContent = currentYear
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};

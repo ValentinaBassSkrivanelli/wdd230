1b6f2f626fc118561736bb93a0c0c90939f8f3f6
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-05-26 18:45:54 -0300

    banner

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -1,6 +1,7 @@
 const options = {weekday: 'long' , day: 'numeric', month: 'long' , year: 'numeric' };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 const option = {weekday: 'long' , day: 'numeric', month: 'long' , year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+
 document.getElementById('current').textContent = new Date().toLocaleDateString('en-US', option);
 const yearSpan = new Date()
 const currentYear = yearSpan.getFullYear();
@@ -16,3 +17,12 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+// banner
+
+const d = new Date();
+if (( d.getDay()  == 1) ||  ( d.getDay()  == 4)) {
+    document.querySelector('#banner').style.display="block"; 
+};
+
+// weather

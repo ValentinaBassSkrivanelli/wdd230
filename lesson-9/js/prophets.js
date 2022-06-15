6f9d474752cd418e058eafe10e43e6d9df5b0c7d
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-06-15 00:21:36 -0300

    .

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -7,28 +7,43 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
     prophets.forEach(displayProphets);
-   
 });
-const prophets = jsonObject['prophets'];
+
+
 function displayProphets(prophet) {
-    // Create elements to add to the document
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let portrait = document.createElement('img');
-  
-    // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = prophet.name + ' ' + prophet.lastname;
-  
-    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    ____.setAttribute('src', prophet.imageurl);
-    ____.setAttribute('alt', 'Portait of ' + prophet.name + ' ' + prophet.lastname);
-    ____.setAttribute('loading', 'lazy');
-  
-    // Add/append the section(card) with the h2 element
-    card.appendChild(h2);
-    card.appendChild(portrait);
-  
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
+  // Create elements to add to the document
+  let card = document.createElement('section');
+  let h2 = document.createElement('h2');
+  let firstP = document.createElement('p');
+  let secondP = document.createElement('p');
+
+  let portrait = document.createElement('img');
+
+  // Change the textContent property of the h2 element to contain the prophet's full name
+ numOrden = "°"
+  if (prophet.death == null){
+    death = "";
+  }else{
+    death = `Death:  ${prophet.death}`;
   }
+
+  h2.textContent = `${prophet.name} ${prophet.lastname} - ${prophet.order}${numOrden} Latter-day President`;
+  firstP.textContent = `Birth and Place: ${prophet.birthdate},  ${prophet.birthplace} `;
+  secondP.textContent = death;
+
+  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+  portrait.setAttribute('src', prophet.imageurl);
+  portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+  portrait.setAttribute('loading', 'lazy');
+
+  // Add/append the section(card) with the h2 element
+  card.appendChild(h2);
+  card.appendChild(firstP);
+  card.appendChild(secondP);
+  card.appendChild(portrait);
+
+  // Add/append the existing HTML div with the cards class with the section(card)
+  document.querySelector('div.cards').appendChild(card);
+}

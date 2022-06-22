1a0ab86d89cdc318480fadd213fce59fb4ef7736
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-06-22 09:52:07 -0300

    js

--- lesson-10/js/weatherapi.js
+++ lesson-10/js/weatherapi.js
@@ -3,4 +3,25 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={3ae5457e97a7d33f682133e2c5262993}';
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=lat&lon=lon&q=Fairbanks&units=imperial&appid=3ae5457e97a7d33f682133e2c5262993';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        //displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+  function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  }

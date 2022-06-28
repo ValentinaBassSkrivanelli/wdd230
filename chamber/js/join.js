bdc3cb8609f13dea0332f4a079f74076fcf2ff6e
valentinabassskrivanelli <valswaonyou@gmail.com>
2022-06-28 10:24:35 -0300

    .

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -3,3 +3,4 @@ document.getElementById('formTime').value = new Date().toString();
 const result = window.localStorage.setItem("storemydata", document.getElementById("formTime").value);
 
 console.log(result)
+ 
